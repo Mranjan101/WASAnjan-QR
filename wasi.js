b3b3eb3624fa36989a71a046e318ab6d1b6de5f8
🌟⭐ ᴹ🅡.ɑ𝖓𝐣ａ𝖓 🌟⭐ <163623222+Mranjan101@users.noreply.github.com>
2024-05-12 15:12:08 +0530

    wasi.js

--- wasi.js
+++ wasi.js
@@ -17,14 +17,13 @@ const { Boom } = require("@hapi/boom");
 const PORT = process.env.PORT ||  5000
 const MESSAGE = process.env.MESSAGE ||  `
 ┌───⭓『
-❒ *WASI-MD*
+❒ *ANJAN-MDv2*
 ❒ _NOW DEPLOY IT_
 └────────────⭓
 ┌───⭓
-❒  • Chat with owner •
-❒ *GitHub:* __https://github.com/WASI-MD_
-❒ *Author:* _wa.me/923192173398_
-❒ *YT:* _https://youtube.com/@wasitech10_
+❒˚Chat with owner˚
+❒ *GitHub:* __https://github.com/Mranjan101_
+❒ *Author:* _wa.me/91988e457657_
 └────────────⭓
 `
 
